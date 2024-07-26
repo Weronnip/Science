a5d14af79fb62d2a0b394766b8b03bcdb4d6a4cb
weronnip <wowo.vovo12345@yandex.ru>
2024-07-26 12:11:21 +0300

    add new link my github

--- src/components/layout/layout.tsx
+++ src/components/layout/layout.tsx
@@ -1,6 +1,6 @@
 import { InputPushDate } from "../form_input/push_date";
 import img from "../../assets/Other 01.png"
-import logo from "../../assets/Logo.png";
+import logo from "../../assets/logo.png";
 import "./layout.css"
 
 export function HeadSite() {
@@ -12,7 +12,7 @@ export function HeadSite() {
                 <li className="item">Пункт 1</li>
                 <li className="item">Пункт 2</li>
                 <li className="item">Пункт 3</li>
-                <li className="item">Пункт 4</li>
+                <li className="item"><a href=" https://github.com/Weronnip" target="_blank">GitHub</a></li>
                 <a href="tel:+78009999999" className="left-[150px] relative top-[5px]
                                                     text-blue-700 text-2xl">+ 7 (800) 999-99-99</a>
             </ul>
